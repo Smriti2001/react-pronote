ebfa5c45f75e21f8a81b9616b59a4dc8d8705421
Smriti <smriti.panda210@gmail.com>
2022-04-18 19:31:48 +0530

    Smriti-Prograd-Batch106

---
+++ pronote/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Pronote from './components/Editor.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Pronote/>
+    </div>
+  );
+}
+
+export default App;
